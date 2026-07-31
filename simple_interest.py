d3e80b67391478706518270d5dc338fcb43f6011
Supriya Dnyaneshwar Mulik <supriyamulik99@gmail.com>
2026-08-01 00:59:21 +0530

    Implement simple interest calculation in Python

---
+++ simple_interest.py
@@ -0,0 +1,7 @@
+principal = float(input("Enter Principal: "))
+rate = float(input("Enter Rate: "))
+time = float(input("Enter Time: "))
+
+si = (principal * rate * time) / 100
+
+print("Simple Interest =", si)
